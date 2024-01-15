57c323b755df6ab0473cb53e4f0e66b4b2fdafb9
akshay-puranik <akshay.social@gmail.com>
2024-01-15 16:13:10 +0530

    setup redux, store, moved initdata to reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 import { TodoList } from "./components/TodoList";
-
-let toDos = [
-  {
-    title: "Complete Project Proposal",
-    description: "Write and submit the project proposal for approval",
-    dueDate: "2024-02-01",
-    status: "pending",
-  },
-  {
-    title: "Prepare for Meeting",
-    description:
-      "Gather necessary documents and information for the client meeting",
-    dueDate: "2024-01-20",
-    status: "pending",
-  },
-  {
-    title: "Review Code Changes",
-    description:
-      "Review and test the recent code changes in the development branch",
-    dueDate: "2024-01-25",
-    status: "pending",
-  },
-  {
-    title: "Send Monthly Report",
-    description:
-      "Compile and send out the monthly progress report to stakeholders",
-    dueDate: "2024-01-31",
-    status: "pending",
-  },
-  {
-    title: "Complete Training Modules",
-    description:
-      "Finish the remaining training modules for professional development",
-    dueDate: "2024-02-10",
-    status: "pending",
-  },
-];
+import { Navbar } from "./components/Navbar";
+import { useSelector } from "react-redux";
 
 function App() {
+  const toDos = useSelector(store => store.todos)
+  const [displayTodos, setDisplayTodos] = useState([...toDos]);
+
+  const handleFilters = ({ selectOrder, selectStatus, searchName }) => {
+    let newList = [...toDos];
+    if (searchName) {
+      newList = newList.filter((task) =>
+        task.title.toLowerCase().includes(searchName)
+      );
+    }
+    if (selectStatus && selectStatus !== "all") {
+      newList = newList.filter((task) => task.status === selectStatus);
+    }
+    // if (selectOrder && selectOrder === "asc") {
+    //   newList.sort((a, b) => a.title - b.title);
+    // }
+    // else{
+    //   newList.sort((a, b) => b.title - a.title);
+    // }
+
+    setDisplayTodos(pre => [...newList]);
+  };
+
   return (
     <>
-      <TodoList tasks={toDos} />
+      <Navbar handleFilters={handleFilters} />
+      <TodoList tasks={displayTodos} />
     </>
   );
 }
---
+++ src/redux/reducer.js
@@ -0,0 +1,45 @@
+export const initState = {
+  todos: [
+    {
+      title: "Complete Project Proposal",
+      description: "Write and submit the project proposal for approval",
+      dueDate: "2024-02-01",
+      status: "pending",
+    },
+    {
+      title: "Prepare for Meeting",
+      description:
+        "Gather necessary documents and information for the client meeting",
+      dueDate: "2024-01-20",
+      status: "pending",
+    },
+    {
+      title: "Review Code Changes",
+      description:
+        "Review and test the recent code changes in the development branch",
+      dueDate: "2024-01-25",
+      status: "pending",
+    },
+    {
+      title: "Send Monthly Report",
+      description:
+        "Compile and send out the monthly progress report to stakeholders",
+      dueDate: "2024-01-31",
+      status: "pending",
+    },
+    {
+      title: "Complete Training Modules",
+      description:
+        "Finish the remaining training modules for professional development",
+      dueDate: "2024-02-10",
+      status: "pending",
+    },
+  ],
+};
+
+export const reducer = (state = initState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
